6a5f2e27e71129c429685646c12d421045a336b0
Michael Schuller <michael.schuller@artlogic.net>
2014-01-17 12:15:42 +0000

    Fix vars.sh typo

--- scripts/vars.sh
+++ scripts/vars.sh
@@ -5,9 +5,11 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 PYTHON="python"
 if hash python2 >/dev/null 2>&1; then
     PYTHON=python2
+fi
 if hash python2.7 >/dev/null 2>&1; then
     PYTHON=python2.7
 fi
+
 MAC_PYTHON27="/Library/Frameworks/Python.framework/Versions/2.7/bin/python2.7"
 if [ -f $MAC_PYTHON27 ]; then
     PYTHON=$MAC_PYTHON27
