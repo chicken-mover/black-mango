d9b11df76eb9526ef96aff0d42a48bb9666729e8
Michael Schuller <michael.schuller@artlogic.net>
2014-02-07 10:20:27 +0000

    Fix automerge happening in the wrong directory

--- scripts/automerge.sh
+++ scripts/automerge.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
+cd $DIR/..
 
-source vars.sh
+source scripts/vars.sh
 
 echo "Performing automatic push to origin/$BRANCH (CTL-C to abort)"
 sleep 2 && git push origin $BRANCH && git push --tags
